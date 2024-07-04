90a5f8038b420120e59a06fc6f9d4ae62f8005e0
daniel.marfo <danielamakyemarfo@gmail.com>
2024-07-04 18:24:54 +0000

    modified the header, footer, job, jobDetail and jobList components

--- src/component/jobDetail.tsx
+++ src/component/jobDetail.tsx
@@ -4,11 +4,11 @@ const JobDetail = ({ ActiveJob, isModal }: {ActiveJob?: JOB,isModal: boolean} ) 
   if (!ActiveJob) return null;
 
   return (
-    <div className={`ActiveJob-detail p-4 max-w-[900px] ${isModal && "hidden md:block"}`}>
-      <h2 className="text-xl font-bold mb-2">{ActiveJob.role}</h2>
-      <p className="mb-2">{ActiveJob.company_name}</p>
-      <div> {ActiveJob.text} </div>
-      <p className="text-gray-500">Posted {ActiveJob.date_posted}</p>
+    <div className={`ActiveJob-detail p-4 max-w-[900px] ${isModal && "hidden md:block border-2 border-purple-500"}`}>
+      <h2 className="text-xl font-bold mb-2">{ActiveJob.title}</h2>
+      <p className="mb-2">{ActiveJob.company}</p>
+      <div> {ActiveJob.summary} </div>
+      <p className="text-gray-500">{ActiveJob.datePosted}</p>
     </div>
   );
 };
